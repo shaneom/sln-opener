6638e751a730cb5ad28a736111767796f0d01512
Shane O'Mahoney <Shane.O'Mahoney@pdms.com>
2017-08-25 16:06:22 +0100

    Minor bug fixes

--- Index.js
+++ Index.js
@@ -11,33 +11,18 @@ function extension(element) {
   return extName === '.sln';
 };
 
-var Registry = require('winreg'),
-    regKey = new Registry({                                       // new operator is optional 
-    hive: Registry.HKCR                                       // open registry hive HKEY_CURRENT_USER 
-    //key:  '\\VisualStudio.DTE.15.0' // key containing autostart programs 
-})
- 
-// list autostart programs 
-regKey.values(function (err, items /* array of RegistryItem */) {
-  if (err)
-    console.log('ERROR: '+err);
-  else
-    for (var i=0; i<items.length; i++)
-      console.log('ITEM: '+items[i].name+'\t'+items[i].type+'\t'+items[i].value);
+fs.readdir(process.cwd(), function(err, files) {    
+    files.filter(extension).forEach(function(value) {
+        menuItems.push(value);
+    });
+
+    if (menuItems.length === 1) {
+        openSolution(menuItems[0]);
+    }
+    else {
+        showMainMenu();
+    }
 });
-
-//fs.readdir(process.cwd(), function(err, files) {    
-//    files.filter(extension).forEach(function(value) {
-//        menuItems.push(value);
-//    });
-
-//    if (menuItems.length === 1) {
-//        openSolution(menuItems[0]);
-//    }
-//    else {
-//        showMainMenu();
-//    }
-//});
 
 function showMainMenu() {
     if (menuItems.length > 0) {
@@ -53,18 +38,22 @@ function showMainMenu() {
     });
 
     menu.question('Select the solution to open? ', function(input) {
-        if ((input-1) < menuItems.length) {
-            openSolution(menuItems[input-1])
+        let menuOption = parseInt(input);
+        if (isNaN(menuOption)) {process.exit();}
+        if ((menuOption-1) < menuItems.length){
+            openSolution(menuItems[menuOption-1])
         } else {
-            process.exit();
-        }            
+            process.exit();            
+        }
     });
 }
 
 function openSolution(solution) {
     const execFile = require('child_process').execFile;
     const child = execFile(process.env['ProgramFiles(x86)'] + '\\Microsoft Visual Studio\\2017\\Enterprise\\Common7\\IDE\\devenv.exe ', [ solution ], (error, stdout, stderr) => {
-        if (error) { throw error; }        
+        if (error) { 
+            console.log("An error occured when trying to open your solution.  Ensure that you have Visual Studio 2017 installed.")
+         }
         if(menu) menu.close();
     });
 }
