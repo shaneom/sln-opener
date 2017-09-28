91e87e47987354e0a9cbc9373d3a0fb1c22aa75d
Shane O'Mahoney <Shane.O'Mahoney@pdms.com>
2017-09-28 13:28:12 +0100

    Added check to see if the user is in administrative mode

--- mainMenu.js
+++ mainMenu.js
@@ -1,33 +1,44 @@
 module.exports.loadSolution = function(menuItems, vspath) {
     const readline = require('readline');
+    const isAdmin = require('is-admin');
     let menu = null;
 
-    if (menuItems.length > 0) {
-        for (var i = 0; i < menuItems.length; i++) {
-            console.log(i + 1 + '. '  + menuItems[i]);
-        }
-    }
-    console.log(menuItems.length + 1 + '. Exit');
-
-    menu = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    menu.question('Select the solution to open? ', function(input) {
-        let menuOption = parseInt(input);
-        if (isNaN(menuOption)) {process.exit();}
-        if ((menuOption-1) < menuItems.length){ 
-            const execFile = require('child_process').execFile;
-            execFile(vspath + '\\devenv.exe ', [ menuItems[menuOption-1] ], (error) => {
-                if (error) { 
-                    console.log('An error occured when trying to open your solution.  Ensure that you have Visual Studio 2017 installed.');
-                    console.log(error);
-                }
-                if(menu) menu.close();
-            });
+    isAdmin().then(admin => {
+        if (admin) {
+            createMenu();
         } else {
-            process.exit();            
+            console.log('You need elevated permission to run this tool.  Please reload your CLI with administrative access.');
         }
     });
+
+    var createMenu = function () {
+        if (menuItems.length > 0) {
+            for (var i = 0; i < menuItems.length; i++) {
+                console.log(i + 1 + '. '  + menuItems[i]);
+            }
+        }
+        console.log(menuItems.length + 1 + '. Exit');
+
+        menu = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        menu.question('Select the solution to open? ', function(input) {
+            let menuOption = parseInt(input);
+            if (isNaN(menuOption)) {process.exit();}
+            if ((menuOption-1) < menuItems.length){ 
+                const execFile = require('child_process').execFile;
+                execFile(vspath + '\\devenv.exe ', [ menuItems[menuOption-1] ], (error) => {
+                    if (error) { 
+                        console.log('An error occured when trying to open your solution.  Ensure that you have Visual Studio 2017 installed.');
+                        console.log(error);
+                    }
+                    if(menu) menu.close();
+                });
+            } else {
+                process.exit();            
+            }
+        });
+    }
 };
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@ A simple command line tool for opening solution files via the console.
 
 It will look for solution files (.sln) and open them. If multiple solution files are found, They will be listed as a menu to pick from.
 
+When this tool initially loads it will ask for the path to your Visual Studio instance.  It also requires you to run your CLI in administrative mode.
+
 ## Install 
 
 ```
