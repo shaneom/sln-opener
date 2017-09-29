358d618c891c42f4ffd685519229ef6f70c60aa3
Shane O'Mahoney <Shane.O'Mahoney@pdms.com>
2017-09-29 17:10:29 +0100

    Fixed bug with main menu.

--- mainMenu.js
+++ mainMenu.js
@@ -2,36 +2,32 @@ module.exports.loadSolution = function(menuItems, vspath) {
     const readline = require('readline');
     let menu = null;
 
-    createMenu();
-
-    var createMenu = function () {
-        if (menuItems.length > 0) {
-            for (var i = 0; i < menuItems.length; i++) {
-                console.log(i + 1 + '. '  + menuItems[i]);
-            }
+    if (menuItems.length > 0) {
+        for (var i = 0; i < menuItems.length; i++) {
+            console.log(i + 1 + '. '  + menuItems[i]);
         }
-        console.log(menuItems.length + 1 + '. Exit');
-
-        menu = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        menu.question('Select the solution to open? ', function(input) {
-            let menuOption = parseInt(input);
-            if (isNaN(menuOption)) {process.exit();}
-            if ((menuOption-1) < menuItems.length){ 
-                const execFile = require('child_process').execFile;
-                execFile(vspath + '\\devenv.exe ', [ menuItems[menuOption-1] ], (error) => {
-                    if (error) { 
-                        console.log('An error occured when trying to open your solution.  Ensure that you have Visual Studio 2017 installed.');
-                        console.log(error);
-                    }
-                    if(menu) menu.close();
-                });
-            } else {
-                process.exit();            
-            }
-        });
     }
+    console.log(menuItems.length + 1 + '. Exit');
+
+    menu = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    menu.question('Select the solution to open? ', function(input) {
+        let menuOption = parseInt(input);
+        if (isNaN(menuOption)) {process.exit();}
+        if ((menuOption-1) < menuItems.length){ 
+            const execFile = require('child_process').execFile;
+            execFile(vspath + '\\devenv.exe ', [ menuItems[menuOption-1] ], (error) => {
+                if (error) { 
+                    console.log('An error occured when trying to open your solution.  Ensure that you have Visual Studio 2017 installed.');
+                    console.log(error);
+                }
+                if(menu) menu.close();
+            });
+        } else {
+            process.exit();            
+        }
+    });
 };
